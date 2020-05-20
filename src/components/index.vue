70c6505066389f84e288d99ba47fb5d7d95cb8c9
zrutao <1585240709@qq.com>
2020-05-20 15:13:32 +0800

    测试

---
+++ src/components/index.vue
@@ -0,0 +1,100 @@
+<template>
+  <el-container class="home_container">
+    <el-header>
+      <div class="home_title">个人博客管理平台</div>
+      <div class="home_userinfoContainer">
+        <el-dropdown @command="handleCommand">
+  <span class="el-dropdown-link home_userinfo">
+    管理员<i class="el-icon-arrow-down el-icon--right home_userinfo"></i>
+  </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item command="sysMsg">系统消息</el-dropdown-item>
+            <el-dropdown-item command="MyArticle">我的文章</el-dropdown-item>
+            <el-dropdown-item command="MyHome">个人主页</el-dropdown-item>
+            <el-dropdown-item command="logout" divided>退出登录</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </div>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">
+        <el-menu
+          default-active="0"
+          class="el-menu-vertical-demo" style="background-color: #ECECEC" router>
+          <template>
+            <el-submenu index="1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>首页</span>
+              </template>
+              <el-menu-item-group>
+                <el-menu-item index="1-1">选项1</el-menu-item>
+                <el-menu-item index="1-2">选项2</el-menu-item>
+              </el-menu-item-group>
+            </el-submenu>
+          </template>
+        </el-menu>
+      </el-aside>
+      <el-container>
+        <el-main>
+          <el-breadcrumb separator-class="el-icon-arrow-right">
+            <el-breadcrumb-item :to="{ path: '/index' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item v-text="this.$router.currentRoute.name"></el-breadcrumb-item>
+          </el-breadcrumb>
+          <keep-alive>
+            <router-view v-if="this.$route.meta.keepAlive"></router-view>
+          </keep-alive>
+          <router-view v-if="!this.$route.meta.keepAlive"></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+</script>
+
+<style>
+  .home_container {
+    height: 100%;
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    width: 100%;
+  }
+
+  .el-header {
+    background-color: #20a0ff;
+    color: #333;
+    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .el-aside {
+    background-color: #ECECEC;
+  }
+
+  .el-main {
+    background-color: #fff;
+    color: #000;
+    text-align: center;
+  }
+
+  .home_title {
+    color: #fff;
+    font-size: 22px;
+    display: inline;
+  }
+
+  .home_userinfo {
+    color: #fff;
+    cursor: pointer;
+  }
+
+  .home_userinfoContainer {
+    display: inline;
+    margin-right: 20px;
+  }
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import index from '@/components/index'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: '首页',
+      component: index,
+      hidden: true,
+    }
+  ]
+})
